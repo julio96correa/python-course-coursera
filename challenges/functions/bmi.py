9f2e2e1557a3ebd1a983666b7ec41635402f024b
julio96correa <julio96correa@gmail.com>
2025-03-09 01:27:16 -0500

    feat(bmi-calculator): add program to calculate BMI(body mass index)

---
+++ challenges/functions/bmi.py
@@ -0,0 +1,22 @@
+def read_number (prompt) -> float:
+    while True:
+        try:
+            number = float( input(prompt).strip())
+            return number
+        except ValueError as e:
+            print(f"Input incorrecto ({e}). Ingrese un número válido")
+
+def get_bmi(weight_lb: float, height_inch: float) -> float:
+    
+    """
+        Function that calculates the body mass index
+    """
+
+    bmi = weight_lb / (height_inch ** 2)
+    return bmi
+
+#main
+weight_lb = read_number("Ingrese el peso en libras: ")       
+height_inch = read_number("Ingrese la altura en pulgadas: ")       
+bmi = get_bmi(weight_lb, height_inch)
+print(f"Su índice de masa corporal es {bmi:.2f}")
